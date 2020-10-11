d128c5775a61997d0f6e0d5d1e2cf5b8ac138264
Andynn <andresperez1024@gmail.com>
2020-10-11 13:50:06 -0500

    First commit. Inicializing one file: proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,10 @@
+"""
+Proyecto integrador
+Materia: Pensamiento computacional para ingeniería
+Alumno: Mauricio Andrés Flores Pérez
+Matrícula: A01639917
+
+Descripción del programa:
+ñalsdkjfñalksdjfñlkasjdfjasñdlfkjañlsk
+"""
+
