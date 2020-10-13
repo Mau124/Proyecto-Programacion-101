5f618813cfb28007f98bf91c7ee6774b21bda63d
Andynn <andresperez1024@gmail.com>
2020-10-12 23:38:13 -0500

    Game session made and userPoitns made

--- proyecto.py
+++ proyecto.py
@@ -9,6 +9,7 @@ Programa que realiza preguntas tipo PISA a estudiantes para mejorar su rendimien
 tipo de examenes.
 """
 import random
+import os
 
 def readQuestions(fileName):
     """Función que lee las preguntas desde un archivo csv y retorna una matriz con ellas"""
@@ -26,9 +27,19 @@ def readQuestions(fileName):
         matrixQuestions.append(row.strip().split(","))
     
     # Imprimiendo la matriz para comprobar que los datos han sido guardados
-    print(*matrixQuestions, sep="\n")
+    # print(*matrixQuestions, sep="\n")
 
     return matrixQuestions
+
+def saveRecord(name, points):
+    """Función que guarda los datos de un usuario"""
+    # Header del archivo
+    headerUsers = "Name,Points\n"
+
+    with open("userPoints.csv", "w") as outputFile:
+        outputFile.write(headerUsers)
+        outputFile.write(name+","+str(points)+"\n")
+
 
 def printQuestion(question):
     """Función que imprime con formato una pregunta"""
@@ -43,20 +54,57 @@ def printQuestion(question):
     
 
 def game():
-    """Función que inicializa una partida de juego y mantiene la sesión de un juego"""
+    """Función que mantiene una sesión de un juego"""
     questions = readQuestions("questions.csv")
+    attemps = 2
+    auxAttemps = 0
     correct = 0
     
-    while correct<10:
-        if len(questions)>1:
-            index = random.randint(0, len(questions)-1)
-        else:
-            index = 0
+    while len(questions)>0:
+        # os.system("cls") sistemas windows
+        os.system("clear") # sistemas unix
+
+        # Si ya no quedan intentos, tomamos otra pregunta aleatoria
+        if auxAttemps<=0:
+            if len(questions)>1:
+                index = random.randint(0, len(questions)-1)
+            else:
+                index = 0
+            auxAttemps = attemps
             
         printQuestion(questions[index])
-        next = input()
-        del questions[index]
-        
-        correct += 1
+        ans = input("¿Coloque el inciso de la respuesta que crea correcta?: ")
+        ans = ans.lower()
+
+        # Checa si la opcion tecleada por el usuario es valida
+        if ans in ("abcd"):
+
+            # Checamos si la respuesta dada por el usuario corresponde a la respuesta correcta
+            if ans==questions[index][7]:
+                print(f"Respuesta Correcta. +{(100/(attemps/auxAttemps)):.2f} puntos")
+                correct += (100/(attemps/auxAttemps))
+                auxAttemps = 0
+                del questions[index]
+            else:
+                auxAttemps -= 1
+                print(f"Respuesta Incorrecta. Te quedan {auxAttemps} intentos para esta pregunta")
+                if auxAttemps<=0:
+                    del questions[index]
+
+        # Checha si la opción tecleada por el usuario es salir del juego
+        elif ans=='s':
+            accept = input("¿Estás seguro de que deseas salir de la aplicación? s/n: ")
+            accept = accept.lower()
+            if accept=='s':
+                break;
+
+        # Checha si la opción tecleada por el usuario no existe
+        else:
+            print("No existe la opción seleccionada. Intente otra vez")
+
+        next = input("Presione enter para continuar: ")
+
+    print(f"Puntos obtenidos: {correct}")
+    saveRecord("Juanito", correct)
 
 game()
